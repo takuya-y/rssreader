68a5304bf22d0606bb6bbfb4d83698dfe02276d2
takuya <sui.shien@gmail.com>
2016-07-04 22:16:46 +0900

    前回アクセス時のデータを消去後、情報更新

--- app/controllers/topic_controller.rb
+++ app/controllers/topic_controller.rb
@@ -1,5 +1,9 @@
 class TopicController < ApplicationController
   def index
+    Topic.delete_all
+    run = Rssreader.new
+    run.rssrun
+    # binding.pry
     @topics = Topic.all
   end
 end
--- app/models/rssreader.rb
+++ app/models/rssreader.rb
@@ -9,18 +9,20 @@ require 'rss'
 # http://rss.itmedia.co.jp/rss/2.0/news_domestic.xml
 # ベンチャー人
 # http://rss.itmedia.co.jp/rss/2.0/news_venture.xml
+class Rssreader
+  def rssrun
+    urls = ["http://rss.itmedia.co.jp/rss/2.0/itmedia_all.xml",
+            "http://rss.itmedia.co.jp/rss/2.0/news_bursts.xml",
+            "http://rss.itmedia.co.jp/rss/2.0/news_domestic.xml",
+            "http://rss.itmedia.co.jp/rss/2.0/news_venture.xml"]
 
-urls = ["http://rss.itmedia.co.jp/rss/2.0/itmedia_all.xml",
-        "http://rss.itmedia.co.jp/rss/2.0/news_bursts.xml",
-        "http://rss.itmedia.co.jp/rss/2.0/news_domestic.xml",
-        "http://rss.itmedia.co.jp/rss/2.0/news_venture.xml"]
+    urls.each do |url|
+      rss = RSS::Parser.parse(url)
 
-urls.each do |url|
-  rss = RSS::Parser.parse(url)
-
-  rss.items.each do |item|
-    product = Topic.where(title: item.title, link: item.link).first_or_initialize
-    product.save
+      rss.items.each do |item|
+        product = Topic.where(title: item.title, link: item.link).first_or_initialize
+        product.save
+      end
+    end
   end
-
 end
